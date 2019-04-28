7706343a9a4a6326a57b8d8bcf94aaa75fc2e847
melikeylldrm1 <melikeylldrm1@gmail.com>
2019-04-28 17:25:29 +0300

    cizim

---
+++ app/src/main/java/com/example/melike/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.melike.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new cizim(this));
+    }
+}
